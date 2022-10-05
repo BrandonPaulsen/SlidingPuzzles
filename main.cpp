ba184d254c1bdf203eab1cb293761beaf81a5509
Poly1581 <bpaul006@ucr.edu>
2022-10-05 11:18:01 -0700

    added code to test getValidMoves

--- main.cpp
+++ main.cpp
@@ -3,4 +3,19 @@
 int main() {
 	game test = game(3);
 	test.display();
+	vector<position> moves = test.getValidMoves();
+	cout << "VALID MOVES:" << endl;
+	for(position move:moves) {
+		if(move == UP) {
+			cout << "\tUP" << endl;
+		} else if(move == RIGHT) {
+			cout << "\tRIGHT" << endl;
+		} else if(move == DOWN) {
+			cout << "\tDOWN" << endl;
+		} else if(move == LEFT) {
+			cout << "\tLEFT" << endl;
+		} else {
+			cout << "INVALID MOVE" << endl;
+		}
+	}
 }
