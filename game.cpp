d7da3af298858c90008cc6c23924aa39770f5e60
Poly1581 <bpaul006@ucr.edu>
2022-10-05 11:01:42 -0700

    added display method

--- game.cpp
+++ game.cpp
@@ -1,4 +1,7 @@
 #include "game.hpp"
+#include <iostream>
+using std::cout;
+using std::endl;
 
 
 //		INITIALIZE ALL GAMES TO SOLVED STATE
@@ -9,7 +12,17 @@ game::game(int size) {
 		for(int col = 0; col < size; col++) {
 			board.back().push_back((size*row)+col+1);
 		}
-		emptySpace = {size-1, size-1};
-		board.at(emptySpace.row).at(emptySpace.col) = 0;
 	}
+	emptySpace = {size-1, size-1};
+	board.at(emptySpace.row).at(emptySpace.col) = 0;
 }
+
+void game::display(void) { 
+	for(int row = 0; row < board.size(); row++) {
+		for(int col = 0; col < board.size(); col++) {
+			cout << board.at(row).at(col) << "\t";
+		}
+		cout << endl << endl;
+	}
+	cout << endl << endl;
+};
