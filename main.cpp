a656d3b96ed327b5db5f746a46d0091e83c2ef88
Poly1581 <bpaul006@ucr.edu>
2022-10-05 16:20:26 -0700

    modified code to test heuristics

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 #include "game.hpp"
 
+
+
 int main() {
+	srand(time(NULL));
 	priority_queue<game> Q;
-	game test = game(3);
-	Q.push(test);
-	vector<game> children = test.getChildren();
-	for(game child:children) {
-		Q.push(child);
-	}
-	while(!Q.empty()) {
-		game curr = Q.top();
-		curr.display();
-		Q.pop();
-	}
+	game solved = game(3);
+	solved.display();
+	game random = game(3);
+	random.randomize();
+	random.display();
+	cout << "UNIFORM COST:\t\t" << random.uniformCost(solved) << endl;
+	cout << "MISPLACED TILE:\t\t" << random.misplacedTile(solved) << endl;
+	cout << "MANHATTAN DISTANCE:\t" << random.manhattanDistance(solved) << endl;
 }
