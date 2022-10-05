d4e8c7fc85621c0e8a038edd0fd100306e44a7c8
Poly1581 <bpaul006@ucr.edu>
2022-10-05 11:49:37 -0700

    added code to test the getchildren method

--- main.cpp
+++ main.cpp
@@ -3,19 +3,8 @@
 int main() {
 	game test = game(3);
 	test.display();
-	vector<position> moves = test.getValidMoves();
-	cout << "VALID MOVES:" << endl;
-	for(position move:moves) {
-		if(move == UP) {
-			cout << "\tUP" << endl;
-		} else if(move == RIGHT) {
-			cout << "\tRIGHT" << endl;
-		} else if(move == DOWN) {
-			cout << "\tDOWN" << endl;
-		} else if(move == LEFT) {
-			cout << "\tLEFT" << endl;
-		} else {
-			cout << "INVALID MOVE" << endl;
-		}
+	vector<game> children = test.getChildren();
+	for(game child:children) {
+		child.display();
 	}
 }
