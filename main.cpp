a6cd0ea2e5a0e229f9e7604e62e12519c2b79f8e
Poly1581 <bpaul006@ucr.edu>
2022-10-05 10:31:11 -0700

    added position struct and directions UP, DOWN, LEFT, RIGHT

--- main.cpp
+++ main.cpp
@@ -1,5 +1,28 @@
+#include <vector>
+using std::vector;
 
+
+#define matrix vector<vector<int>>
+
+struct position {
+	int row = 0;
+	int col = 0;
+	friend bool operator==(const position& a, const position& b) {
+		return (a.row == b.row) && (a.col == b.col);
+	}
+	position operator+(const position& d) {
+		position p;
+		p.row = this->row + d.row;
+		p.col = this->col + d.col;
+		return p;
+	}
+};
+
+const position UP = {-1,0};
+const position RIGHT = {0,1};
+const position DOWN = {1,0};
+const position LEFT = {0,-1};
 
 int main() {
-	
+
 }
