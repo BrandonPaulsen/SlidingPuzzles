78ab80e340edb536a0aae5c3bc18e77342bb0544
Poly1581 <bpaul006@ucr.edu>
2022-10-06 13:54:05 -0700

    modified main for testing

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
 #include "game.hpp"
 
 
-
 int main() {
 	srand(time(NULL));
 	priority_queue<game> Q;
 	game solved = game(3);
 	solved.display();
-	game random = game(3);
-	random.randomize();
-	random.display();
-	cout << "UNIFORM COST:\t\t" << random.uniformCost(solved) << endl;
-	cout << "MISPLACED TILE:\t\t" << random.misplacedTile(solved) << endl;
-	cout << "MANHATTAN DISTANCE:\t" << random.manhattanDistance(solved) << endl;
+	vector<game> children = solved.getChildren();
+	for(game child:children) {
+		child.display();
+	}
+	// game random = game(3);
+	// random.randomize();
+	// random.display();
+	// cout << "UNIFORM COST:\t\t" << random.uniformCost(solved) << endl;
+	// cout << "MISPLACED TILE:\t\t" << random.misplacedTile(solved) << endl;
+	// cout << "MANHATTAN DISTANCE:\t" << random.manhattanDistance(solved) << endl;
 }
